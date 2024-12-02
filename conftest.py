7bbba5a5405e0bfd66f658015815b48174aa3510
VERA_POPOVA <vera.popova@simbirsoft.com>
2024-12-02 12:29:51 +0300

    Добавляем тесты авторизации

--- conftest.py
+++ conftest.py
@@ -8,6 +8,6 @@ def driver():
     options = Options()
     options.add_argument('--enable-javascript')
     _driver = webdriver.Chrome(options=options)
-    _driver.get('https://www.saucedemo.com')
+    _driver.get('https://www.saucedemo.com/')
     yield _driver
     _driver.quit()
